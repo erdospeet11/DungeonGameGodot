c0058ed3358a5fca2b6bb73e6ba226aaf4b13e3d
erdospeet11 <90241347+erdospeet11@users.noreply.github.com>
2024-03-08 13:33:50 +0100

    Added grid-based movement

---
+++ CharacterBody2D.gd
@@ -0,0 +1,43 @@
+extends Area2D
+
+var animation_speed = 2
+var moving = false
+var tile_size = 16
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+
+@onready var ray = $RayCast2D
+@onready var animator = $animatedsprite
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			print(dir)
+			if dir == "right":
+				animator.flip_h = false
+			if dir == "left":
+				animator.flip_h = true
+			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		animator.play("walk")
+		await tween.finished
+		animator.stop()
+		moving = false
